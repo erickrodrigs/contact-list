988c9323e2b649dc3dcc84b446707a74576e49cd
erickrodrigs <erick.rodrigues.santana14@gmail.com>
2020-07-28 00:12:50 -0300

    add form structure

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,25 @@
 <template>
   <v-app>
-    <v-main>
+    <div class="d-flex justify-center flex-wrap mx-auto my-auto" style="width: 80%;">
+      <aside class="mr-6" style="">
+        <ContactForm />
+      </aside>
+      <v-main width="300px" style="background-color: #303030">
       
-    </v-main>
+      
+      </v-main>
+    </div>
   </v-app>
 </template>
 
 <script>
+import ContactForm from './components/ContactForm';
 
 export default {
   name: 'App',
+  components: {
+    ContactForm
+  },
   data: () => ({
     //
   }),
---
+++ src/components/ContactForm.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-form>
+    <v-text-field prepend-inner-icon="mdi-account pb-1 mr-1" outlined placeholder="Name"></v-text-field>
+    <v-text-field prepend-inner-icon="mdi-calendar pb-1 mr-1" outlined placeholder="Age"></v-text-field>
+    <v-text-field prepend-inner-icon="mdi-email pb-1 mr-1" outlined placeholder="Email" type="email"></v-text-field>
+    <v-text-field prepend-inner-icon="mdi-phone pb-1 mr-1" outlined placeholder="Phone"></v-text-field>
+    <v-btn block x-large depressed color="success">Cadastrar</v-btn>
+  </v-form>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
