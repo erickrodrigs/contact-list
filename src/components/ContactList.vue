12e335e410205077b740bdc7765ba751b8e5540f
erickrodrigs <erick.rodrigues.santana14@gmail.com>
2020-07-29 00:35:45 -0300

    feat message when contact list is empty

--- src/components/ContactList.vue
+++ src/components/ContactList.vue
@@ -1,24 +1,30 @@
 <template>
-  <v-list 
-    three-line 
+  <div 
+    class="d-flex"
     style="border: 1px solid gray; height: 396px; width: 600px; overflow-y:auto">
-    <template
-      v-for="contact in contacts">
-      <v-list-item v-bind:key="contact.email">
-        <v-list-item-avatar>
-          <v-icon>mdi-account-circle</v-icon>
-        </v-list-item-avatar>
-        <v-list-item-content>
-          <v-list-item-title>{{ contact.name }}, {{ contact.age }} years old</v-list-item-title>
-          <v-list-item-subtitle>Email: {{ contact.email }}</v-list-item-subtitle>
-        </v-list-item-content>
-        <v-list-item-icon style="cursor: pointer" v-on:click="removeContact(contact)">
-          <v-icon>mdi-delete</v-icon>
-        </v-list-item-icon>
-      </v-list-item>
-      <v-divider v-bind:key="contact.phone"></v-divider>
-    </template>
-  </v-list>
+    <p v-if="contacts.length === 0" class="mx-auto my-auto">No contacts yet...</p>
+    <v-list
+      v-if="contacts.length !== 0"
+      width="100%"
+      three-line>
+      <template
+        v-for="contact in contacts">
+        <v-list-item v-bind:key="contact.email">
+          <v-list-item-avatar>
+            <v-icon>mdi-account-circle</v-icon>
+          </v-list-item-avatar>
+          <v-list-item-content>
+            <v-list-item-title>{{ contact.name }}, {{ contact.age }} years old</v-list-item-title>
+            <v-list-item-subtitle>Email: {{ contact.email }}</v-list-item-subtitle>
+          </v-list-item-content>
+          <v-list-item-icon style="cursor: pointer" v-on:click="removeContact(contact)">
+            <v-icon>mdi-delete</v-icon>
+          </v-list-item-icon>
+        </v-list-item>
+        <v-divider v-bind:key="contact.phone"></v-divider>
+      </template>
+    </v-list>
+  </div>
 </template>
 
 <script>
--- src/store.js
+++ src/store.js
@@ -5,32 +5,7 @@ Vue.use(Vuex);
 
 const store = new Vuex.Store({
   state: {
-    contacts: [
-      {
-        name: 'Carlos',
-        age: 24,
-        email: 'carlos@ime.usp.br',
-        phone: '(11) 94569-2087'
-      },
-      {
-        name: 'Edna',
-        age: 49,
-        email: 'edna@ime.usp.br',
-        phone: '(11) 11111-1111'
-      },
-      {
-        name: 'Rogério',
-        age: 33,
-        email: 'rogerio@ime.usp.br',
-        phone: '(11) 22222-2222'
-      },
-      {
-        name: 'Marcos',
-        age: 31,
-        email: 'marcos@ime.usp.br',
-        phone: '(11) 33333-3333'
-      }
-    ]
+    contacts: []
   },
   getters: {
     getContacts: state => state.contacts
