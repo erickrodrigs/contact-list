3f4ca2b306a0206bbe5b4cddcd335456b0454287
erickrodrigs <erick.rodrigues.santana14@gmail.com>
2020-07-30 23:07:56 -0300

    add vue router

--- src/store/index.js
+++ src/store/index.js
@@ -11,17 +11,11 @@ const store = new Vuex.Store({
     getContacts: state => state.contacts
   },
   mutations: {
-    addContact: (state, contact) => {
-      state.contacts.push(contact);
-    },
-    removeContact: (state, index) => {
-      state.contacts.splice(index, 1);
-    }
+    addContact: (state, contact) => state.contacts.push(contact),
+    removeContact: (state, index) => state.contacts.splice(index, 1)
   },
   actions: {
-    addContact: (context, contact) => {
-      context.commit('addContact', contact);
-    },
+    addContact: (context, contact) => context.commit('addContact', contact),
     removeContact: (context, contact) => {
       const index = context.state.contacts.findIndex(elem => elem.email === contact.email);
       context.commit('removeContact', index);
---
+++ src/views/Home.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-app >
+    <div class="d-flex flex-wrap justify-center mx-auto my-auto">
+      <aside class="mr-6">
+        <ContactForm />
+      </aside>
+      <v-main>
+        <ContactList />
+      </v-main>
+    </div>
+  </v-app>
+</template>
+
+<script>
+import ContactForm from '../components/ContactForm';
+import ContactList from '../components/ContactList';
+
+export default {
+  name: 'Home',
+  components: {
+    ContactForm,
+    ContactList
+  },
+  data: () => ({
+    //
+  })
+};
+</script>
